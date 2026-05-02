726d57e36021ac1d9ba6a8202f27fc019d4ff2c7
jtawari095 <jtawari095@gmail.com>
2026-05-02 03:23:15 +0000

    tlck: nix: introduce nix devshell derivation

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+	# Description
+	description = "Secrets Manager";
+
+	# Inputs
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	# Outputs
+	outputs = {self, nixpkgs, flake-utils, ...}: flake-utils.lib.eachDefaultSystem(system: let
+		# Packages
+		pkgs = import nixpkgs {
+			inherit system;
+		};
+	in {
+		# Devshells
+		devShells.default = pkgs.mkShell {
+			name = "tlck";
+			nativeBuildInputs = with pkgs; [
+				wrapGAppsHook4
+				gobject-introspection
+			];
+			packages = with pkgs; [
+				gtk4
+				cairo
+				pkg-config
+				libadwaita
+			];
+		};
+	});
+}
